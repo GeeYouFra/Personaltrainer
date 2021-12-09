6cebe98ca92378166bdc5b02052bf23a30e3902a
Giovani Francisco <giovani.francisco@myy.haaga-helia.fi>
2021-12-09 23:46:48 +0200

    Updated version

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -1,40 +1,109 @@
-import React, { useState, useEffect } from 'react';
-import { AgGridReact } from 'ag-grid-react';
+import React, { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import AddCustomer from "./AddCustomer";
 
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-material.css';
-
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-material.css";
 
 function Customerlist() {
-    const [customers, setCustomers] = useState([]);
-    useEffect(() => {
-        fetch('https://customerrest.herokuapp.com/api/customers')
-            .then(response => response.json())
-            .then(data => setCustomers(data.content))
-            .catch(err => console.error(err))
-    }, [])
+  const [customers, setCustomers] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [msg, setMsg] = useState("");
 
-    const columns = [
-        { field: 'firstname', width: 150, sortable: true, filter: true },
-        { field: 'lastname', width: 150, sortable: true, filter: true },
-        { field: 'email', width: 200, sortable: true, filter: true },
-        { field: 'phone', width: 150, sortable: true, filter: true },
-        { field: 'streetaddress', sortable: true, filter: true },
-        { field: 'postcode', width: 150, sortable: true, filter: true },
-        { field: 'city', width: 150, sortable: true, filter: true },
-    ]
+  useEffect(() => {
+    fetchCustomers();
+  }, []);
 
-    return (
-        <div className="ag-theme-material" style={{ height: 600, width: '90%', margin: 'auto' }}>
-            <AgGridReact
-                rowData={customers}
-                columnDefs={columns}
-                pagination={true}
-                paginationPageSize={10}
-            />
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-        </div>
-    )
+  const fetchCustomers = () => {
+    fetch("https://customerrest.herokuapp.com/api/customers")
+      .then((response) => response.json())
+      .then((data) => setCustomers(data.content))
+      .catch((err) => console.error(err));
+  };
+
+  const deleteCustomer = (url) => {
+    if (window.confirm("Do you really want to DELETE me?")) {
+      fetch(url, { method: "DELETE" })
+        .then((response) => {
+          if (response.ok) {
+            setMsg("A tired trainee left the group");
+            setOpen(true);
+            fetchCustomers();
+          } else {
+            alert("Oops! Something went wrong");
+          }
+        })
+        .catch((err) => console.error(err));
+    }
+  };
+
+  const addCustomer = (customer) => {
+    fetch("https://customerrest.herokuapp.com/api/customers", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(customer),
+    })
+      .then((response) => fetchCustomers())
+      .catch((err) => console.error(err));
+  };
+
+  const columns = [
+    { field: "firstname", width: 150, sortable: true, filter: true },
+    { field: "lastname", width: 150, sortable: true, filter: true },
+    { field: "email", width: 200, sortable: true, filter: true },
+    { field: "phone", width: 150, sortable: true, filter: true },
+    { field: "streetaddress", sortable: true, filter: true },
+    { field: "postcode", width: 150, sortable: true, filter: true },
+    { field: "city", width: 150, sortable: true, filter: true },
+    {
+      headerName: " ",
+      sortable: false,
+      filter: false,
+      width: 120,
+      field: "links[1].href",
+      cellRendererFramework: (params) => (
+        <Button
+          size="small"
+          color="error"
+          onClick={() => deleteCustomer(params.data.links[1].href)}
+        >
+          Delete
+        </Button>
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      <AddCustomer addCustomer={addCustomer} />
+      <div
+        className="ag-theme-material"
+        style={{ height: 600, width: "90%", margin: "auto" }}
+      >
+        <AgGridReact
+          rowData={customers}
+          columnDefs={columns}
+          pagination={true}
+          paginationPageSize={10}
+          suppressCellSelection={true}
+        />
+      </div>
+      <Snackbar
+        open={open}
+        message={msg}
+        autoHideDuration={3000}
+        onClose={handleClose}
+      />
+    </div>
+  );
 }
 
 export default Customerlist;
--- src/components/Traininglist.js
+++ src/components/Traininglist.js
@@ -1,45 +1,117 @@
-import React, { useState, useEffect } from 'react';
-import { AgGridReact } from 'ag-grid-react';
+import React, { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import AddTraining from "./AddTraining";
 
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-material.css';
-import { format } from 'date-fns'
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-material.css";
+import { format } from "date-fns";
 
 function Traininglist() {
-    const [trainings, setTrainings] = useState([]);
-    const fullname = (trainings) => {
-        return `${trainings.data.customer.firstname} ${trainings.data.customer.lastname}`
+  const [trainings, setTrainings] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [msg, setMsg] = useState("");
+  const fullname = (trainings) => {
+    return `${trainings.data.customer.firstname} ${trainings.data.customer.lastname}`;
+  };
+  const fulldate = (trainings) => {
+    const formatDate = format(new Date(trainings.data.date), "dd.MM.yyy HH:mm");
+    return formatDate;
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    fetchTrainings();
+  }, []);
+
+  const fetchTrainings = () => {
+    fetch("https://customerrest.herokuapp.com/gettrainings")
+      .then((response) => response.json())
+      .then((data) => setTrainings(data))
+      .catch((err) => console.error(err));
+  };
+
+  const deleteTraining = (link) => {
+    if (window.confirm("Sure to delete this one?")) {
+      fetch(
+        "https://customerrest.herokuapp.com/api/trainings/" + link.data.id,
+        { method: "DELETE" }
+      )
+        .then((response) => {
+          if (response.ok) {
+            setMsg("Successful! See you next time");
+            setOpen(true);
+            fetchTrainings();
+          } else {
+            alert("Oops! Something went wrong");
+          }
+        })
+        .catch((err) => console.error(err));
     }
-    const fulldate = (trainings) => {
-        const formatDate = format(new Date(trainings.data.date), 'dd.MM.yyy HH:mm')
-        return formatDate
-    }
+  };
 
-    useEffect(() => {
-        fetch('https://customerrest.herokuapp.com/gettrainings')
-            .then(response => response.json())
-            .then(data => setTrainings(data))
-            .catch(err => console.error(err))
-    }, [])
+  const columns = [
+    {
+      valueGetter: fulldate,
+      headerName: "Date",
+      width: 150,
+      sortable: true,
+      filter: true,
+    },
+    { field: "activity", width: 150, sortable: true, filter: true },
+    { field: "duration", width: 200, sortable: true, filter: true },
+    {
+      valueGetter: fullname,
+      headerName: "Customer",
+      width: 150,
+      sortable: true,
+      filter: true,
+    },
+    {
+      headerName: " ",
+      sortable: false,
+      filter: false,
+      width: 120,
+      field: "links[1].href",
+      cellRendererFramework: (params) => (
+        <Button
+          size="small"
+          color="error"
+          onClick={() => deleteTraining(params)}
+        >
+          Delete
+        </Button>
+      ),
+    },
+  ];
 
-    const columns = [
-        { valueGetter: fulldate, headerName: 'Date', width: 150, sortable: true, filter: true },
-        { field: 'activity', width: 150, sortable: true, filter: true },
-        { field: 'duration', width: 200, sortable: true, filter: true },
-        { valueGetter: fullname, headerName: 'Customer', width: 150, sortable: true, filter: true },
-    ]
-
-    return (
-        <div className="ag-theme-material" style={{ height: 600, width: '90%', margin: 'auto' }}>
-            <AgGridReact
-                rowData={trainings}
-                columnDefs={columns}
-                pagination={true}
-                paginationPageSize={10}
-            />
-
-        </div>
-    )
+  return (
+    <div>
+      <AddTraining />
+      <div
+        className="ag-theme-material"
+        style={{ height: 600, width: "90%", margin: "auto" }}
+      >
+        <AgGridReact
+          rowData={trainings}
+          columnDefs={columns}
+          pagination={true}
+          paginationPageSize={10}
+          suppressCellSelection={true}
+        />
+        <Snackbar
+          open={open}
+          message={msg}
+          autoHideDuration={3000}
+          onClose={handleClose}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Traininglist;
