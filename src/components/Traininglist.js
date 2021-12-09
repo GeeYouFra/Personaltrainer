b1add089ab7a4acecfbf04796ca6681323e5d73a
Giovani Francisco <giovani.francisco@myy.haaga-helia.fi>
2021-12-09 13:55:16 +0200

    Personal Trainer First

---
+++ src/components/Traininglist.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import { format } from 'date-fns'
+
+function Traininglist() {
+    const [trainings, setTrainings] = useState([]);
+    const fullname = (trainings) => {
+        return `${trainings.data.customer.firstname} ${trainings.data.customer.lastname}`
+    }
+    const fulldate = (trainings) => {
+        const formatDate = format(new Date(trainings.data.date), 'dd.MM.yyy HH:mm')
+        return formatDate
+    }
+
+    useEffect(() => {
+        fetch('https://customerrest.herokuapp.com/gettrainings')
+            .then(response => response.json())
+            .then(data => setTrainings(data))
+            .catch(err => console.error(err))
+    }, [])
+
+    const columns = [
+        { valueGetter: fulldate, headerName: 'Date', width: 150, sortable: true, filter: true },
+        { field: 'activity', width: 150, sortable: true, filter: true },
+        { field: 'duration', width: 200, sortable: true, filter: true },
+        { valueGetter: fullname, headerName: 'Customer', width: 150, sortable: true, filter: true },
+    ]
+
+    return (
+        <div className="ag-theme-material" style={{ height: 600, width: '90%', margin: 'auto' }}>
+            <AgGridReact
+                rowData={trainings}
+                columnDefs={columns}
+                pagination={true}
+                paginationPageSize={10}
+            />
+
+        </div>
+    )
+}
+
+export default Traininglist;
